c0e2b9bfef8b42a8d22f073911ed23588e92f593
Virtual-Click <virtual-click@learniosfreecoms-mac.local>
2016-10-19 14:39:54 -0600

    Initial Commit

---
+++ Hola Mundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hola Mundo
+//
+//  Created by Virtual-Click on 10/19/16.
+//  Copyright © 2016 Virtual-Click. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
