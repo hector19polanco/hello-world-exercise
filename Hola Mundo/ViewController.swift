b4981efa62836bf7e9a01fda2b51aeb71d07a5a4
Virtual-Click <virtual-click@learniosfreecoms-mac.local>
2016-10-21 03:04:15 -0600

    commit message for example

--- Hola Mundo/ViewController.swift
+++ Hola Mundo/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var personalData: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeMessage(sender: AnyObject) {
+        messageLabel.text = "Hola \(nameTextField.text)"
+        view.endEditing(true)
+    }
 
+    @IBAction func changeName(sender: AnyObject) {
+        personalData.text = "Hector"
+        view.endEditing(true)
+    }
+    
+    @IBAction func changeLastname(sender: AnyObject) {
+        personalData.text = "Polanco"
+        view.endEditing(true)
+    }
+    
+    @IBAction func changeCity(sender: AnyObject) {
+        personalData.text = "Ciudad"
+        view.endEditing(true)
+    }
+    
+    @IBAction func changeCountry(sender: AnyObject) {
+        personalData.text = "Guatemala"
+        view.endEditing(true)
+    }
+    
 }
 
